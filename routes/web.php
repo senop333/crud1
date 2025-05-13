087090e55a74e27959828b70e5bcc2e5df1e32bf
senop333 <senop333@gmail.com>
2025-05-13 12:20:55 +0800

    membuat tabel sql

--- routes/web.php
+++ routes/web.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('pages.index');
-});
+})->name('home');
 
 Route::get('/hello',function(){
     return view('hello word');
