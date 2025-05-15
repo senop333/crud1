536ee154046ae90063224a5551b669f9be97156f
senop333 <senop333@gmail.com>
2025-05-15 08:39:45 +0800

    require

--- resources/views/pages/presence/create.blade.php
+++ resources/views/pages/presence/create.blade.php
@@ -22,14 +22,29 @@
                     <div class="mb3">
                         <label for="nama_kegiatan">Nama Kegiatan</label>
                         <input type="text" class="form-control" name="nama_kegiatan" id="nama_kegiatan" value="{{ old('nama_kegiatan') }}">
+                        @error('nama_kegiatan')
+                            <div class="text-danger">
+                                {{ $message }}
+                            </div>
+                        @enderror
                     </div>
                     <div class="mb3">
                         <label for="tgl_kegiatan">Tanggal Kegiatan</label>
                         <input type="date" class="form-control" name="tgl_kegiatan" id="tgl_kegiatan" value="{{ old('tgl_kegiatan') }}">
+                        @error('tgl_kegiatan')
+                            <div class="text-danger">
+                                {{ $message }}
+                            </div>
+                        @enderror
                     </div>
                     <div class="mb3">
                         <label for="waktu_mulai">Waktu Mulai</label>
                         <input type="time" class="form-control" name="waktu_mulai" id="waktu_mulai" value="{{ old('waktu_mulai') }}">
+                        @error('waktu_mulai')
+                            <div class="text-danger">
+                                {{ $message }}
+                            </div>
+                        @enderror
                     </div>
                     <button type="submit" class="btn btn-primary">
                         Simpan
